89e5d5415800b74487c9d78a6ae47b534452ffb9
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-01-25 18:37:31 -0300

    Update curriculo-em-site.py

--- CV/curriculo-em-site.py
+++ CV/curriculo-em-site.py
@@ -1,126 +1,71 @@
 import streamlit as st
-import time
+import pandas as pd
 
 # Configuração da página
 st.set_page_config(
     page_title="João Carvalho",
-    page_icon="😁",
-    layout="centered",
+    page_icon="🙂",
+    layout="wide",
     initial_sidebar_state="auto"
 )
 
-# Estilo personalizado
-st.markdown(
-    """
-    <style>
-    .main-title { color: #2C3E50; font-size: 32px; font-weight: bold; text-align: center; }
-    .section-title { color: #3498DB; font-size: 24px; margin-top: 20px; }
-    .profile-pic { display: block; margin: 0 auto 10px; border-radius: 50%; }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+col1, col2 = st.columns([4, 1])
+    
+with col1:
+    st.title("João Marcos Santos e Carvalho")
+    st.write("Estudante de Ciência da Computação, com paixão por tecnologia, programação e análise de dados.")
+        
 
-# Tabulação
-tab1, tab2 = st.tabs(["Currículo", "Outros Conteúdos"])
+        
+    st.divider()
+    st.subheader(":blue[PROJETOS]")
+    st.write("**[Projeto Currículo](https://curriculo-joaocarvalho.streamlit.app/)** — _Projeto Python_")
+    st.caption("Objetivo de criar esse currículo em um site usando a biblioteca Streamlit do Python. ")
+    st.write("**Projeto Guia do Universitário** — _Projeto Integrador_")
+    st.caption("Plataforma web para ajudar calouros com dicas sobre a faculdade e estudos.")
 
-with tab1:
-    # Colunas dentro da aba 1
-    col1, col2 = st.columns([4, 1])
 
-    with col1:
-        st.markdown('<h1 class="main-title">João Marcos Santos e Carvalho</h1>', unsafe_allow_html=True)
-        st.write("Estudante de Engenharia de Software. Interesse em Ciência de Dados e Inteligência Artificial.")
+    st.divider()
+    st.subheader(":blue[FORMAÇÃO]")
+    st.write("Instituto de Educação Superior de Brasília (IESB), Asa Sul, DF")
+    st.caption("Bacharelado em Ciência da Computação - Quarto semestre, com previsão de formatura em 2026.")
 
-        st.image("fotodeperfil.png", caption="João Carvalho", width=150, use_container_width=True)
+    st.divider()
 
-        st.divider()
-        st.markdown('<h2 class="section-title">EXPERIÊNCIA PROFISSIONAL</h2>', unsafe_allow_html=True)
-        st.write("Em construção. Aguardando oportunidade de estágio em TI.")
+    st.subheader(":blue[REDES]")
 
-        st.divider()
-        st.markdown('<h2 class="section-title">PROJETOS</h2>', unsafe_allow_html=True)
-        st.write("**[Projeto Currículo](https://(colocar ainda).streamlit.app)**")
-        st.caption("Criação de um currículo interativo usando Streamlit.")
+    button_col1, button_col2, button_col3, button_col4 = st.columns([1, 1, 1, 1])
 
-        st.divider()
-        st.markdown('<h2 class="section-title">FORMAÇÃO</h2>', unsafe_allow_html=True)
+    with button_col1:
+        st.link_button("🐈‍⬛Github", "https://github.com/dev-joaocarvalho") 
 
-        st.write("Instituto de Educação Superior de Brasília (IESB), Asa Sul, DF")
-        st.caption("Bacharelado em Ciência da Computação - Quarto semestre, com previsão de formatura em 2026.")
+    with button_col2:
+        st.link_button("🌎LinkedIn", "https://www.linkedin.com/in/joaom-s-carvalho/")
+          
 
-        st.write("Universidade de Brasília (UnB), Gama, DF")
-        st.caption("Bacharelado em Engenharia de Software - Primeiro semestre, com previsão de formatura em 2029.")
+with col2:
+    st.caption("Brasília, DF, 73105-905")
+    st.caption("**☎️ +55 61 98199-4401**")
+    st.caption("**dev.joaocarvalho@gmail.com**")
+    st.link_button("🐈‍⬛GitHub", "https://github.com/dev-joaocarvalho") 
+    st.link_button("🌎LinkedIn", "https://www.linkedin.com/in/joaom-s-carvalho/")
+        
+    st.divider()
+    st.write(":blue[COMPETÊNCIAS]")
+    st.caption("Programação: C, Java, Python.")
+    st.caption("Análise de Dados: Excel, Python (Pandas, NumPy), SQL.")
+    st.caption("Desenvolvimento Web: HTML, CSS, Streamlit.")
+    st.caption("Ferramentas: Eclipse, Visual Studio Code, Spring Boot.")
+    st.caption("Banco de dados: DBeaver.")
+    st.caption("Sistemas Operacionais: Linux, Windows.")
 
-        st.divider()
-        st.markdown('<h2 class="section-title">REDES</h2>', unsafe_allow_html=True)
-
-        button_col1, button_col2 = st.columns([1, 1])
-
-        with button_col1:
-            st.button("🐱 GitHub", key="github_button")
-
-        with button_col2:
-            st.button("🔗 LinkedIn", key="linkedin_button")
-
-    with col2:
-        st.caption("Grande Colorado, DF, 73105-905")
-        st.caption("**☎️ +55 61 08199-4401**")
-        st.caption("**dev.joaocarvalho@gmail.com**")
-
-        st.divider()
-        st.write("**:blue[COMPETÊNCIAS]**")
-        st.caption("Programação: Java, Python, C.")
-        st.caption("Análise de Dados: Excel, Python (Pandas, NumPy), SQL.")
-        st.caption("Desenvolvimento Web: HTML, CSS, Streamlit.")
-        st.caption("Ferramentas: Eclipse, Visual Studio Code.")
-        st.caption("Gestão de Projetos: Scrum, Kanban, Microsoft Project, Trello.")
-        st.caption("Pacote Office: Word, Excel, PowerPoint.")
-        st.caption("Sistemas Operacionais: Linux, Windows.")
-
-        st.divider()
-        st.write("**:blue[IDIOMAS]**")
-        st.caption("Inglês.")
+    st.divider()
+    st.write(":blue[PROJETOS VOLUNTÁRIOS]")
+    st.caption("Monitoria em Tópicos de Matemática")
+        
+    st.divider()
+    st.write(":blue[IDIOMAS]")
+    st.caption("Inglês Intermediário.")
 
 st.divider()
-
-# URL do arquivo PDF no GitHub
-pdf_url = "https://raw.githubusercontent.com/CraraMaria/Testes_Curr%C3%ADculo/main/Curr%C3%ADculo_JoaoCarvalho.pdf"  # Atualize com o link correto
-
-# Cria o botão de download
-st.markdown(f'<a href="{pdf_url}" download="Currículo_JoaoCarvalho.pdf">Baixar em PDF</a>', unsafe_allow_html=True)
-
-with tab2:
-    # Conteúdo para a aba 2
-    st.markdown('<h2 class="main-title">PÁGINA EM CONSTRUÇÃO 🛠👩‍🚒</h2>', unsafe_allow_html=True)
-    st.write("Mais sobre mim")
-    st.image("fotodeperfil.png", caption="João Carvalho", width=200, use_container_width=True)
-
-    st.markdown('<h2 class="section-title">Sobre Mim</h2>', unsafe_allow_html=True)
-    st.write(
-        """Sou João Marcos Santos e Carvalho, um estudante de tecnologia sempre em busca de aprender algo novo. 
-Meu objetivo é me tornar um desenvolvedor de destaque, com interesse especial em Ciência de Dados, Inteligência Artificial e Desenvolvimento Full Stack."""
-    )
-
-    st.markdown('<h2 class="section-title">Projetos</h2>', unsafe_allow_html=True)
-    st.write(
-        """**Projeto Currículo**
-Este é um projeto feito com Streamlit, onde criei um currículo interativo para treinar minhas habilidades de desenvolvimento web. 
-[Veja o código no GitHub](https://github.com/dev-joaocarvalho/curriculo-em-site)"""
-    )
-
-    st.markdown('<h2 class="section-title">Contato</h2>', unsafe_allow_html=True)
-    with st.form(key='contact_form'):
-        name = st.text_input("Nome")
-        email = st.text_input("Email")
-        message = st.text_area("Mensagem")
-        submit = st.form_submit_button("Enviar")
-        if submit:
-            with st.spinner("Enviando sua mensagem..."):
-                time.sleep(2)
-                st.success("Mensagem enviada com sucesso!")
-
-                st.success("Mensagem enviada com sucesso!")
-
-    
 
